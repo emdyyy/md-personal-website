ec7b27653a0af0ab755d89fe8fd86d5d499aa26e
Mateusz Dettlaff <mateusz.dettlaff0@gmail.com>
2021-05-14 23:53:35 +0200

    Added comming soon text and tailwind config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,14 @@
 import Head from "next/head";
-import Image from "next/image";
 
 const Home = () => {
   return (
-    <div>
+    <div className="grid place-content-center h-screen">
       <Head>
         <title>Mateusz Dettlaff - Home</title>
         <meta name="description" content="Mateusz Dettlaff Personal Website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="h-40 w-40 relative">
-        <Image
-          src={"/images/md.png"}
-          alt="Picture of the author"
-          layout="fill"
-          objectFit="cover"
-          className="rounded-full"
-        />
-      </div>
+      <h1 className="font-gilroy-bold text-primary text-5xl">coming soon...</h1>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+      'gilroy-bold': ['Gilroy ExtraBold', 'sans-serif'],
+      'gilroy-light': ['Gilroy Light', 'sans-serif']
+      },
+      colors: {
+        primary: {
+          DEFAULT: '#152F4A'
+        },
+      }
+    },
   },
   variants: {
     extend: {},
